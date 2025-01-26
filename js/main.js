bb9f5a2a9736dc1f1ea34b54758b7513f731c817
MOhamed-Kamal11 <m7maaa82@gmail.com>
2025-01-26 16:25:30 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+let title = document.getElementById('title')
+let price = document.getElementById("price")
+let taxes = document.getElementById("taxes")
+let ads = document.getElementById("ads")
+let discount = document.getElementById("discount")
+let total = document.getElementById("total")
+let count = document.getElementById("count")
+let category = document.getElementById("category")
+let submit = document.getElementById("submit")
+
+
+//? ==========GetTotal===========
+function getTotal() {
+  if (price.value != '') {
+    let result = (+price.value + +taxes.value + +ads.value) - +discount.value
+    total.innerHTML = result
+    total.style.background = "#040"
+  } else {
+    total.style.background = '#bd0505'
+    total.innerHTML = ''
+  }
+}
+price.addEventListener('keyup', function (e) {
+  getTotal()
+})
+taxes.addEventListener('keyup', function (e) {
+  getTotal()
+})
+ads.addEventListener('keyup', function (e) {
+  getTotal()
+})
+discount.addEventListener('keyup', function (e) {
+  getTotal()
+})
