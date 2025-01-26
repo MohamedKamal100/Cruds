36905366baa044b74bc51e588f8afbc8a0f32729
MOhamed-Kamal11 <m7maaa82@gmail.com>
2025-01-26 16:59:41 +0200

    create clear data function for make inputs empty after adding product

--- js/main.js
+++ js/main.js
@@ -50,13 +50,26 @@ submit.onclick = function () {
     taxes: taxes.value,
     ads: ads.value,
     discount: discount.value,
-    total: total.value,
+    total: total.innerHTML,
     count: count.value,
     category: category.value,
 
   }
   dataProduct.push(newProduct)
   localStorage.setItem('product', JSON.stringify(dataProduct))
+  clearData()
   console.log(dataProduct)
 }
-// ?========================================
+
+// ?==================clear inputs====================
+function clearData() {
+  title.value = '';
+  price.value = '';
+  taxes.value = '';
+  ads.value = '';
+  discount.value = '';
+  total.innerHTML = '';
+  count.value = '';
+  category.value = '';
+}
+// ?================Read=============================
